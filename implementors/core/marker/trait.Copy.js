82e93b97344468d3872a25ee26e8f61b8877809d
saschagrunert <saschagrunert@users.noreply.github.com>
2021-01-20 07:53:01 +0000

    deploy: 6ce9ba14f9c4b5b98fb3b01643231c8ecd99b094

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["containrs"] = [{"text":"impl Copy for LogScope","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
